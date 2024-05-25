168bb94b2fd0096c6cf57d75c42c93230316de82
heisenberger69 <dhruvm22@iitk.ac.in>
2024-05-25 21:06:30 +0530

    done for 1d
    working just fine for 1d but we have to take g into account

---
+++ lineartrackbad/lineartrackbad.ino
@@ -0,0 +1,69 @@
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  //setupt motion detection
+  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  mpu.setMotionDetectionThreshold(1);
+  mpu.setMotionDetectionDuration(20);
+  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  mpu.setInterruptPinPolarity(true);
+  mpu.setMotionInterrupt(true);
+
+  Serial.println("");
+  delay(100);
+}
+
+
+
+
+
+float vx = 0;
+float sx = 0;
+
+float delaytime = 10;
+
+
+float xvel(float ax)
+{
+  vx = vx + ax * delaytime / 1000;
+  return vx;
+}
+
+float xdisplacement(float sx, float ax)
+{
+  sx = sx + xvel(ax) * delaytime / 1000;
+  return sx;
+}
+void loop() {
+
+  if(mpu.getMotionInterruptStatus()) {
+    /* Get new sensor events with the readings */
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+  // }
+
+ 
+  Serial.println(xdisplacement(sx,a.acceleration.x));
+  }
+
+  delay(delaytime);
+}
